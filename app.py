4ad071634a8d6767541c11481f8766d1d8a3768e
kiranss777 <kiranss458@gmail.com>
2025-05-23 04:11:02 +0530

    FastAPI endpoint modified

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 import sys
 import os
+sys.path.append(os.path.abspath("src"))
+
+import os
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
